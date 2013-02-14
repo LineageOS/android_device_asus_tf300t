7ae5de942c53a4327ed930bbbfd5178b170f3c19
XpLoDWilD <xplodgui@gmail.com>
2013-02-14 23:35:44 +0000

    tf300t: Import new AutoParts from tf700t
    Change-Id: I57153a531647e235e5032df84c3da8acda700415

--- AutoParts/src/com/cyanogenmod/settings/device/DeviceSettings.java
+++ AutoParts/src/com/cyanogenmod/settings/device/DeviceSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 The CyanogenMod Project
+ * Copyright (C) 2013 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,54 +16,98 @@
 
 package com.cyanogenmod.settings.device;
 
+import android.app.ActionBar;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
-import android.os.SystemProperties;
+import android.preference.PreferenceFragment;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.view.MenuItem;
 
-public class DeviceSettings extends PreferenceActivity implements
-        Preference.OnPreferenceChangeListener {
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
 
-    private static final String PREFERENCE_CPU_MODE = "cpu_settings";
-    private static final String CPU_PROPERTY = "sys.cpu.mode";
-
-    private ListPreference mCpuMode;
+public class DeviceSettings extends PreferenceActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        // This activity is always called from another activity, so we
+        // assume that HOME_UP button should be always displayed
+        getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
-
-        String mCurrCpuMode = "1";
-
-        if (SystemProperties.get(CPU_PROPERTY) != null)
-            mCurrCpuMode = SystemProperties.get(CPU_PROPERTY);
-
-        mCpuMode = (ListPreference) getPreferenceScreen().findPreference(
-                PREFERENCE_CPU_MODE);
-
-        mCpuMode.setValueIndex(getCpuModeOffset(mCurrCpuMode));
-        mCpuMode.setOnPreferenceChangeListener(this);
     }
 
-    private int getCpuModeOffset(String mode) {
-        if (mode.equals("0")) {
-            return 0;
-        } else if (mode.equals("2")) {
-            return 2;
-        } else {
-            return 1;
-        }
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.preferences_header, target);
     }
 
-    public boolean onPreferenceChange(Preference preference, Object value) {
-        if (preference.equals(mCpuMode)) {
-            final String newCpuMode = (String) value;
-            SystemProperties.set(CPU_PROPERTY, newCpuMode);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+       switch (item.getItemId()) {
+          case android.R.id.home:
+              finish();
+              return true;
+          default:
+             return super.onOptionsItemSelected(item);
+       }
+    }
+
+    public static class CpuSettingsFragment
+        extends PreferenceFragment implements OnPreferenceChangeListener {
+
+        private ListPreference mCpuMode;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.preferences_cpu);
+
+            String currCpuMode = CpuUtils.CPU_SETTING_PERFORMANCE;
+            if (CpuUtils.getCpuMode() != null) {
+                currCpuMode = CpuUtils.getCpuMode();
+            }
+
+            mCpuMode = (ListPreference)findPreference(CpuUtils.PREFERENCE_CPU_MODE);
+            mCpuMode.setValueIndex(getCpuModeIndex(currCpuMode));
+            mCpuMode.setOnPreferenceChangeListener(this);
         }
 
-        return true;
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String key = preference.getKey();
+            if (key.compareTo(CpuUtils.PREFERENCE_CPU_MODE) == 0) {
+                final String newCpuMode = (String)newValue;
+                CpuUtils.setCpuMode(newCpuMode);
+            }
+            return true;
+        }
+
+        private int getCpuModeIndex(String mode) {
+            if (mode.equals(CpuUtils.CPU_SETTING_POWER_SAVE)) {
+                return Integer.parseInt(CpuUtils.CPU_SETTING_POWER_SAVE);
+            }
+            if (mode.equals(CpuUtils.CPU_SETTING_PERFORMANCE)) {
+                return Integer.parseInt(CpuUtils.CPU_SETTING_PERFORMANCE);
+            }
+            return Integer.parseInt(CpuUtils.CPU_SETTING_BALANCED);
+        }
     }
 
 }
