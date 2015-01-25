8def9d425ed69d40db9f700f8742009d838eb2c3
chrmhoffmann <chrmhoffmann@gmail.com>
2015-01-25 14:49:52 +0100

    tf300t: telephony codecs not needed.
    Change-Id: Iea8717b9e76b575c5f0aa30865b93999bc0af2f3

--- device_tf300t.mk
+++ device_tf300t.mk
@@ -91,7 +91,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # media files
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     device/asus/tf300t/media_codecs.xml:system/etc/media_codecs.xml \
